c74287b4dd2ce8e790eab1a8e256c3d1a77320c1
정혁조 <dy960508@naver.com>
2026-01-06 18:52:11 +0900

    DIP 복습

--- src/main/java/Head13_/NotificationSender.java
+++ src/main/java/Head13_/NotificationSender.java
@@ -4,11 +4,6 @@ public interface NotificationSender {
     void send(String message);
 }
 
-public class SmsNotificationSender implements NotificationSender {
-    public void send(String message) {
-        System.out.println("SMS 전송: " + message);
-    }
-}
 
 
 
