b2c6a5c45b668b3ad487ca3b2d917decc0126bf4
정혁조 <dy960508@naver.com>
2026-01-20 21:39:13 +0900

    알고리즘 직렬화

--- src/main/java/Head_21/ObjectWriteExample.java
+++ src/main/java/Head_21/ObjectWriteExample.java
@@ -7,7 +7,7 @@ import java.io.ObjectOutputStream;
 public class ObjectWriteExample {
     public static void main(String[] args) {
         User user = new User("Alice", 30);
-
+// 직렬화
         try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("user.ser"))) {
             oos.writeObject(user);
             System.out.println("직렬화 완료: user.ser");
